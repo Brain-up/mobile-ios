465bf780ee6501a4e4eb1b269c996cd2bdcf2140
Evgenii Zhigunov <evgenii_zhigunov@epam.com>
2022-03-10 18:28:21 +0500

    add KeychainSwift add Token singleton update authVM

--- BrainUp/BrainUp/AppDelegate.swift
+++ BrainUp/BrainUp/AppDelegate.swift
@@ -16,15 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
         self.window = UIWindow(frame: UIScreen.main.bounds)
         let navController = UINavigationController()
         coordinator = AppCoordinator(navController)
         coordinator?.start()
         window?.rootViewController = navController
         window?.makeKeyAndVisible()
-        
-        FirebaseApp.configure()
-        
         return true
     }
 
