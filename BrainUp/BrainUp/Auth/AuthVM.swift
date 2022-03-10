07c7945442e8a5587bd504888db4c0a29dd2d6e0
Evgenii Zhigunov <evgenii_zhigunov@epam.com>
2022-03-10 18:28:21 +0500

    add KeychainSwift add Token singleton update authVM

--- BrainUp/BrainUp/Auth/AuthVM.swift
+++ BrainUp/BrainUp/Auth/AuthVM.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseCore
+import FirebaseAuth
 
 protocol AuthVMProtocol: AnyObject {
     func auth(login: String, password: String)
@@ -14,16 +16,39 @@ protocol AuthVMProtocol: AnyObject {
 
 class AuthVM: AuthVMProtocol {
     private weak var view: BasicViewInterface?
+    private var authStateHandle: AuthStateDidChangeListenerHandle!
     
     init(view: BasicViewInterface) {
         self.view = view
+        authStateHandle = Auth.auth().addStateDidChangeListener { _, user in
+            if user != nil {
+                user?.getIDToken(completion: { token, error in
+                    if error == nil {
+                        Token.shared.save(token ?? "")
+                    }
+                })
+            }
+        }
     }
     
     func auth(login: String, password: String) {
-        
+        Auth.auth().signIn(withEmail: login, password: password) {[weak self] _, error in
+            if error != nil {
+                self?.view?.showError(errorMessage: error?.localizedDescription)
+            }
+        }
+                           
     }
     
     func register(login: String, password: String) {
-        
+        Auth.auth().createUser(withEmail: login, password: password) {[weak self] _, error in
+            if error != nil {
+                self?.view?.showError(errorMessage: error?.localizedDescription)
+            }
+        }
+    }
+    
+    deinit {
+        Auth.auth().removeStateDidChangeListener(authStateHandle)
     }
 }
